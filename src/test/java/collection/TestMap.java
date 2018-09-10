bc439ba7e2436212c290a32d32aa4f6742272b57
xuwenjin <563210736@qq.com>
2018-09-10 09:16:57 +0800

    测试HashMap扩容

--- src/test/java/collection/TestMap.java
+++ src/test/java/collection/TestMap.java
@@ -1,11 +1,7 @@
 package collection;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import jre8Test.Dish;
 
 /**
  * 测试HashMap的工作原理
@@ -14,24 +10,16 @@ import jre8Test.Dish;
  */
 public class TestMap {
 
+	/**
+	 * threshold 阈值。当table=={}时，该值为初始容量（初始容量默认为16）；当table被填充了，也就是为table分配内存空间后，threshold一般为
+	 * capacity*loadFactory。当超过阈值时，table容量会自动扩大两倍
+	 * 
+	 * initialCapacity 初始化容量(会向上取最接近2的幂次，如10，则初始容量会变成16)
+	 * 
+	 * loadFactor 负载因子，代表了table的填充度有多少
+	 * 
+	 */
 	public static void main(String[] args) {
-		List<Dish> menu = new ArrayList<Dish>();
-		for (int i = 0; i < 10; i++) {
-			Dish dish = new Dish();
-			dish.setCalories(i);
-			if (i % 3 == 0) {
-				dish.setName("鸡肉");
-			} else if (i % 3 == 1) {
-				dish.setName("鸭肉");
-			} else {
-				dish.setName("鱼");
-			}
-			menu.add(dish);
-		}
-		testMap(menu);
-	}
-
-	public static void testMap(List<Dish> menu) {
 		Map<Integer, String> dishMap = new HashMap<>(10, 0.6F);
 		for (int i = 0; i < 100; i++) {
 			dishMap.put(i, i + "");
