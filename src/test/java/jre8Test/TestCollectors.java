5153204b6852038ad3504c108a9967856b0e8bd2
xuwenjin <563210736@qq.com>
2018-09-17 12:38:09 +0800

    修改收集器使用demo

--- src/test/java/jre8Test/TestCollectors.java
+++ src/test/java/jre8Test/TestCollectors.java
@@ -17,19 +17,23 @@ import org.junit.Test;
  */
 public class TestCollectors {
 
-	/**
-	 * 集合去重(将集合根据每个元素的id进行去重)
-	 */
-	@SuppressWarnings("unchecked")
-	@Test
-	public void testDistinctListById() {
-		List<Dish> dishList = new ArrayList<Dish>();
+	private static List<Dish> dishList = new ArrayList<Dish>();
+
+	static {
 		Dish dish1 = new Dish("001", "张三");
 		dishList.add(dish1);
 		Dish dish2 = new Dish("001", "李四");
 		dishList.add(dish2);
 		Dish dish3 = new Dish("002", "王五");
 		dishList.add(dish3);
+	}
+
+	/**
+	 * 集合去重(将集合根据每个元素的id进行去重)
+	 */
+	@SuppressWarnings("unchecked")
+	@Test
+	public void testDistinctListById() {
 		dishList.forEach(d -> System.out.println("id:" + d.getId() + ", name:" + d.getName()));
 		// id:001, name:张三
 		// id:001, name:李四
@@ -44,25 +48,41 @@ public class TestCollectors {
 	}
 
 	/**
-	 * 测试Collectors.toCollection方法：将结果收集到其它类型的集合中(这里是TreeSet)
-	 * 
 	 * 测试TreeSet默认比较器及传入比较器
 	 */
 	@Test
-	public void testToCollection() {
+	public void testTreeSet() {
 		// TreeSet不传比较器，则默认按照自然顺序排序
+		TreeSet<Integer> treeSet = new TreeSet<>();
+		treeSet.add(3);
+		treeSet.add(1);
+		treeSet.add(4);
+		System.out.println(treeSet);
+		// [1, 3, 4]
+
+		// TreeSet中的元素，如果是实体，必须得传比较器(或者实体类需要实现Comparable中的compareTo方法)，不然就会报错
+		// TreeSet是Set的子类，里面的元素有序且不能重复，可以去重
+		TreeSet<Dish> treeSet2 = new TreeSet<>(Comparator.comparing(Dish::getId));
+		treeSet2.addAll(dishList);
+		treeSet2.forEach(d -> System.out.println("id:" + d.getId() + ", name:" + d.getName()));
+		// id:001, name:张三
+		// id:002, name:王五
+
+		List<Dish> newDishList = new ArrayList<>(treeSet2);
+		newDishList.forEach(d -> System.out.println("id:" + d.getId() + ", name:" + d.getName()));
+		// id:001, name:张三
+		// id:002, name:王五
+	}
+
+	/**
+	 * 测试Collectors.toCollection方法：将结果收集到其它类型的集合中(这里是TreeSet)
+	 */
+	@Test
+	public void testToCollection() {
 		TreeSet<Integer> treeSet = Stream.of(3, 1, 4).collect(Collectors.toCollection(TreeSet::new));
 		System.out.println(treeSet);
-		// 1、3、4
+		// [1, 3, 4]
 
-		List<Dish> dishList = new ArrayList<Dish>();
-		Dish dish1 = new Dish("001", "张三");
-		dishList.add(dish1);
-		Dish dish2 = new Dish("001", "李四");
-		dishList.add(dish2);
-		Dish dish3 = new Dish("002", "王五");
-		dishList.add(dish3);
-		// TreeSet中的元素，如果是实体，必须得传比较器(或者实体类需要实现Comparable中的compareTo方法)，不然就会报错
 		TreeSet<Dish> treeSet2 = dishList.stream()
 				.collect(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(Dish::getId))));
 		treeSet2.forEach(d -> System.out.println("id:" + d.getId() + ", name:" + d.getName()));
@@ -71,7 +91,7 @@ public class TestCollectors {
 	}
 
 	/**
-	 * 测试Collectors.collectingAndThen方法：将流中的数据通过Collector计算，最终的结果在通过Function再最终处理一下
+	 * 测试Collectors.collectingAndThen方法：将流中的数据通过Collector计算，计算的结果再通过Function处理一下
 	 * (这里是将TreeSet转为ArrayList。即相当于将最终结果又经过了new ArrayList<>(treeSet))
 	 */
 	@SuppressWarnings("unchecked")
