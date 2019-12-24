64d43cafa1a92fbf69c443a6354d0ec987593e67
xuwenjin <563210736@qq.com>
2019-12-24 13:48:40 +0800

    增加策略模式与工厂模式结合

--- src/main/java/com/xwj/strategy/Context.java
+++ src/main/java/com/xwj/strategy/Context.java
@@ -1,16 +1,46 @@
 package com.xwj.strategy;
 
 import com.xwj.strategy.sub.IBaseStrategy;
+import com.xwj.strategy.sub.StrategyA;
+import com.xwj.strategy.sub.StrategyB;
+import com.xwj.strategy.sub.StrategyC;
 
-import lombok.AllArgsConstructor;
-
-@AllArgsConstructor
 public class Context {
 
 	/**
 	 * 可更换的策略，传入不同的策略对象，做不同的业务
 	 */
 	private IBaseStrategy strategy;
+
+	/**
+	 * 策略模式
+	 * 
+	 * @param strategy
+	 */
+	public Context(IBaseStrategy strategy) {
+		this.strategy = strategy;
+	}
+
+	/**
+	 * 策略模式与工厂模式结合
+	 * 
+	 * @param funcode
+	 */
+	public Context(String funcode) {
+		switch (funcode) {
+		case "001":
+			this.strategy = new StrategyA();
+			break;
+		case "002":
+			this.strategy = new StrategyB();
+			break;
+		case "003":
+			this.strategy = new StrategyC();
+			break;
+		default:
+			break;
+		}
+	}
 
 	public void execute() {
 		strategy.print();
--- src/main/java/com/xwj/strategy/Test.java
+++ src/main/java/com/xwj/strategy/Test.java
@@ -7,6 +7,9 @@ public class Test {
 	public static void main(String[] args) {
 		Context context = new Context(new StrategyA());
 		context.execute();
+		
+		Context context2 = new Context("002");
+		context2.execute();
 	}
 
 }
