8075a80b01897e4b8c3eea02643a81842bc4828a
xuwenjin <563210736@qq.com>
2021-01-18 10:52:21 +0800

    修改最长子串算术题

--- src/test/java/leetcode/LongestSubstring.java
+++ src/test/java/leetcode/LongestSubstring.java
@@ -2,7 +2,6 @@ package leetcode;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -10,41 +9,69 @@ import java.util.Set;
  */
 class LongestSubstring {
 
+	public static void main(String[] args) {
+		// String str = "abc";
+		// String str = "abcabcbb";
+		// String str = "cdd";
+		String str = "abba";
+		System.out.println(test(str));
+	}
+
 	/**
-	 * 给定一个字符串，找到最长不含有重复字符的子字符串
+	 * 解题分析：
+	 * 1、滑动窗口。
+	 * 其实就是一个队列，比如abcbcba，进入这个队列（窗口）为 abc 满足题目要求，当再进入 b，队列变成了 abcb，这时不满足要求。所以，我们要移动这个队列。
+	 * 移动队列方法：将队列中第一次出现b及b之前的所有元素移除
 	 */
-	public static int lengthOfLongestSubstring(String s) {
-		Set<Character> chars = new HashSet<>();
+	public static int test(String str) {
+		Set<Character> set = new HashSet<>();
+		int len = str.length();
+		if (len <= 1) {
+			return len;
+		}
+
 		int max = 0;
-		for (int i = 0, j = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			while (chars.contains(c)) {
-				chars.remove(s.charAt(j++));
+		int left = 0;
+		for (int i = 0; i < len; i++) {
+			char c = str.charAt(i);
+			while (set.contains(c)) {
+				// 记录最近出现的重复元素的下标+1。并删除当前重复元素及之前的所有元素
+				set.remove(str.charAt(left++));
 			}
-			chars.add(c);
-			max = Math.max(max, i - j + 1);
+			set.add(c);
+			max = Math.max(max, set.size());
 		}
 		return max;
 	}
 
-	public static int lengthOfLongestSubstring2(String s) {
-		int n = s.length(), ans = 0;
-		Map<Character, Integer> map = new HashMap<>();
-		// try to extend the range [i, j]
-		for (int j = 0, i = 0; j < n; j++) {
-			if (map.containsKey(s.charAt(j))) {
-				i = Math.max(map.get(s.charAt(j)), i);
-			}
-			ans = Math.max(ans, j - i + 1);
-			map.put(s.charAt(j), j + 1);
+	/**
+	 * 解题分析：
+	 * 1、滑动窗口。
+	 * 其实就是一个队列，比如abcbcba，进入这个队列（窗口）为 abc 满足题目要求，当再进入 b，队列变成了 abcb，这时不满足要求。所以，我们要移动这个队列。
+	 * 移动队列方法：将队列中第一次出现b及b之前的所有元素移除
+	 */
+	public static int test2(String str) {
+		HashMap<Character, Integer> map = new HashMap<>();
+		int len = str.length();
+		if (len <= 1) {
+			return len;
 		}
-		return ans;
-	}
 
-	public static void main(String[] args) {
-		String s = "abcacbb";
-		int maxLen = lengthOfLongestSubstring2(s);
-		System.out.println(maxLen);
+		int max = 0;
+		int left = 0;
+		for (int i = 0; i < len; i++) {
+			char c = str.charAt(i);
+			if (map.containsKey(c)) {
+				// 记录最近出现的重复元素的下标+1
+				// 这里使用Math.max，是为了防止当前重复元素的下标比现有重复元素小的情况。比如abba
+				left = Math.max(left, map.get(c) + 1);
+			}
+			map.put(c, i);
+
+			// 计算两个下标间子字符串长度时，是没有算边界的，所以这里的+1
+			max = Math.max(max, i - left + 1);
+		}
+		return max;
 	}
 
 }
